06a53baed1c6eee9896098bf0c888122b7f1eae3
akhlakm <me@akhlakm.com>
2024-03-28 13:26:46 -0400

    Enable docker service

--- setup.sh
+++ setup.sh
@@ -71,6 +71,7 @@ nft list ruleset | tee -a /etc/sysconfig/nftables.conf
 yum install -y yum-utils
 yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
 dnf install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+systemctl enable docker
 
 echo -e "\nDone! Please reboot and reconnect:"
 echo -e "\tssh $AN_USER@$HOSTNAME -p $AN_PORT"
