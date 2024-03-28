8ac3a2821d31a98d4a7ebd7705e3849519561555
akhlakm <me@akhlakm.com>
2024-03-28 14:40:34 -0400

    Make setup script lean

--- setup.sh
+++ setup.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
+# Script to setup a new machine for ansible:
+#       SSHD, Firewalld and Docker
+#
+# Tested on: AlmaLinux 9.0
+# ==============================================================================
+
 set -e          # exit on error
 set -x          # show the command being executed
+
+# Update and install the system packages
+# --------------------------------------
+dnf update -y
+dnf install -y epel-release git firewalld
 
 # Ansible user account and sshd port
 # ----------------------------------
@@ -12,10 +23,6 @@ AN_PORT=${AN_PORT:-22}
 
 echo "Setting up system for user: $AN_USER port: $AN_PORT"
 
-# Update the system packages
-# ----------------------------------
-dnf update -y
-
 # Enable NOPASSWD for the wheel/root group
 echo "%wheel  ALL=(ALL)       NOPASSWD: ALL" >> /etc/sudoers
 
@@ -26,8 +33,13 @@ usermod -a -G wheel ${AN_USER}
 echo "Please setup a password for the ansible user:"
 passwd ${AN_USER}
 
-# Change sshd port to <ansible_port>
+# Setup SSHD
+# ----------------------------------
+# Change sshd port
 echo Port $AN_PORT >> /etc/ssh/sshd_config
+
+# Change sshd port of firewalld
+sed "/port/{s|22|$AN_PORT|}" /usr/lib/firewalld/services/ssh.xml > /etc/firewalld/services/ssh.xml
 
 # Create ansible user ssh directory
 mkdir /home/${AN_USER}/.ssh
@@ -38,27 +50,19 @@ cp /root/.ssh/authorized_keys /home/${AN_USER}/.ssh/ || echo "No ssh key found."
 # Update permission
 chown ${AN_USER} /home/${AN_USER}/.ssh/authorized_keys || echo "Please add authorized_keys manually."
 
-# Install other packages
-# ----------------------------------
-dnf install -y epel-release dnf-automatic
-dnf install -y firewalld fail2ban
-
 # Setup Docker
 # ----------------------------------
 yum install -y yum-utils
 yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
 dnf install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
-# Change sshd port of firewalld
+# Enable services
 # ----------------------------------
-sed "/port/{s|22|$AN_PORT|}" /usr/lib/firewalld/services/ssh.xml > /etc/firewalld/services/ssh.xml
-
-# Setup Services
-# ----------------------------------
-systemctl enable fail2ban
-systemctl enable firewalld
-systemctl enable dnf-automatic-install.timer
 systemctl enable docker
+systemctl enable firewalld
 
+# Complete!
+# ----------------------------------
 echo -e "\nDone! Please reboot and reconnect:"
 echo -e "\tssh $AN_USER@$HOSTNAME -p $AN_PORT"
+exit 0
